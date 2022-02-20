f388426db296e6bf4d73e632f123f09e5f2b4a52
mohammedmasood871 <mohammedmasood871@gmail.com>
2022-02-20 21:43:01 +0530

    1 file modified

--- src/app/books-view/books-view.component.ts
+++ src/app/books-view/books-view.component.ts
@@ -1,28 +1,22 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { MatPaginator } from '@angular/material/paginator';
 import { ApiServiceService } from '../service/api-service.service';
 
 @Component({
   selector: 'app-books-view',
   templateUrl: './books-view.component.html',
-  styleUrls: ['./books-view.component.css']
+  styleUrls: ['./books-view.component.css'],
 })
 export class BooksViewComponent implements OnInit {
-  booksData:any[] = [];
-  @ViewChild(MatPaginator)
-    paginator!: MatPaginator;
-  constructor( private apiService:ApiServiceService){}
-  ngOnInit(): void {  
+  booksData: any[] = [];
 
-  }
+  constructor(private apiService: ApiServiceService) {}
+  ngOnInit(): void {}
 
-  getBooks(val:any){
-    console.log(val, 'from search')
-    this.apiService.getBooks(val).subscribe((res:any) =>{
+  getBooks(val: any) {
+    console.log(val, 'from search');
+    this.apiService.getBooks(val).subscribe((res: any) => {
       this.booksData = res.docs;
-      console.log(res)
-    })
-
+      console.log(res);
+    });
   }
-
 }
